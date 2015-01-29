6f2cb3a13d75dc3d43b04d17295c08b1e5f72c0a
Callum White <callumw1991@gmail.com>
2015-01-29 15:38:14 +1000

    Updated leiningen profile

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,2 +1,3 @@
 {:user {:plugins [[cider/cider-nrepl "0.8.2"]
-                  [refactor-nrepl "0.2.2"]]}}
+                  [refactor-nrepl "0.2.2"]
+                  [joodie/clojure-refactoring "0.6.4"]]}}
