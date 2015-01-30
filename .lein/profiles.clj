300603eb53503269b3b049bee42b6dd9b1aa025b
Callum White <callumw1991@gmail.com>
2015-01-30 17:54:46 +1100

    Added ultra library to lein profile

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,5 @@
 {:user {:plugins [[cider/cider-nrepl "0.8.2"]
                   [refactor-nrepl "0.2.2"]
-                  [joodie/clojure-refactoring "0.6.4"]]}}
+                  [joodie/clojure-refactoring "0.6.4"]
+                  [venantius/ultra "0.1.9"]]
+        :ultra {:color-scheme :solarized_dark}}}
