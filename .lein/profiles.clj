1460614f175aa2ce2fac9661da484fe93a81e147
Callum White <callumw1991@gmail.com>
2015-02-03 01:08:47 +1100

    Commented out Ultra dependency because it was messing with simple-brepl

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,8 @@
 {:user {:plugins [[cider/cider-nrepl "0.8.2"]
                   [refactor-nrepl "0.2.2"]
                   [joodie/clojure-refactoring "0.6.4"]
-                  [venantius/ultra "0.1.9"]
-                  [lein-bikeshed "0.2.0"]]
-        :ultra {:color-scheme :solarized_dark}}}
+               ;; [venantius/ultra "0.1.9"]
+                  [lein-bikeshed "0.2.0"]
+                  [lein-marginalia "0.8.0"]]
+        :ultra {:color-scheme :solarized_dark}}
+ :cuttle {:plugins [[lein-pprint "1.1.1"]]}}
