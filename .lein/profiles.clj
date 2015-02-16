b34b0c978131b0667806872f73f8e884a2706e43
callum white <callumw1991@gmail.com>
2015-02-16 17:15:57 +1100

    Ultra was giving me issues in a work project, so it's getting the flick for now. Couldn't be bothered investigating it. Simply disabling all ultra components didn't fix the issue so I've commented out the whole dependency.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,9 +1,12 @@
 {:user {:plugins [[cider/cider-nrepl "0.8.2"]
                   [refactor-nrepl "0.2.2"]
                   [joodie/clojure-refactoring "0.6.4" :exclusions [org.clojure/clojure]]
-                  [venantius/ultra "0.1.9"]
+                  ;; [venantius/ultra "0.1.9"]
                   [lein-bikeshed "0.2.0"]
                   [lein-marginalia "0.8.0"]]
         :ultra {:repl false
+                :stacktraces false
+                :tests false
+                :java false
                 :color-scheme :solarized_dark}}
  :cuttle {:plugins [[lein-pprint "1.1.1"]]}}
