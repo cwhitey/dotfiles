ad950fc31985c330cf3deb5a00b82daf6d43cf4b
callum white <callum.white@auspost.com.au>
2015-04-02 16:36:35 +1100

    Updated lein profile

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,7 @@
 {:user {:plugins [[lein-ancient "0.6.2"]
                   [cider/cider-nrepl "0.8.2"]
+                  ;; This snapshot contains middleware for cider-debug
+                  ;; [cider/cider-nrepl "0.9.0-SNAPSHOT"]
                   [refactor-nrepl "0.2.2"]
                   [joodie/clojure-refactoring "0.6.4" :exclusions [org.clojure/clojure]]
                   ;; [venantius/ultra "0.1.9"]
