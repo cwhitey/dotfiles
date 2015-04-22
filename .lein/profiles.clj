a8cb60819998e3a7d20992d6eef8d2b2c7b5dfde
Callum White <callumw1991@gmail.com>
2015-04-22 21:31:19 +1000

    lein-droid added to profiles, and ultra uncommented

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -4,11 +4,13 @@
                   ;; [cider/cider-nrepl "0.9.0-SNAPSHOT"]
                   [refactor-nrepl "0.2.2"]
                   [joodie/clojure-refactoring "0.6.4" :exclusions [org.clojure/clojure]]
-                  ;; [venantius/ultra "0.1.9"]
+                  [venantius/ultra "0.3.3"]
                   [lein-bikeshed "0.2.0"]
                   [lein-marginalia "0.8.0"]
-                  [lein-try "0.4.3"]]
-        :ultra {:repl false
+                  [lein-try "0.4.3"]
+                  [lein-droid "0.4.0-alpha1"] ; Clojure android project builder
+                  ]
+        :ultra {:repl true
                 :stacktraces false
                 :tests false
                 :java false
