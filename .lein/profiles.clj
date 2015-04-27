aac1c14cf4f9f5b56719284a9e6cd4abd416498a
Callum White <callumw1991@gmail.com>
2015-04-27 10:59:46 +1000

    Updated profiles.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,8 +1,9 @@
 {:user {:plugins [[lein-ancient "0.6.2"]
+                  [com.cemerick/piggieback "0.2.0"]
                   [cider/cider-nrepl "0.8.2"]
                   ;; This snapshot contains middleware for cider-debug
                   ;; [cider/cider-nrepl "0.9.0-SNAPSHOT"]
-                  [refactor-nrepl "0.2.2"]
+                  [refactor-nrepl "1.0.3"]
                   [joodie/clojure-refactoring "0.6.4" :exclusions [org.clojure/clojure]]
                   [venantius/ultra "0.3.3"]
                   [lein-bikeshed "0.2.0"]
