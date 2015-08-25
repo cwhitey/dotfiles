2479a9f1eb34737f897c1bd25f6a0d6d42d2ae94
Callum White <callum.white@auspost.com.au>
2015-08-25 20:43:16 +1000

    Commented out some dependencies which were seriously slowing down clojure startup time. This can be included in specific projects if needed. Also rolled back to a refactor-nrepl versions which didn't throw an exception when running up a repl in a non-project directory.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,15 +1,15 @@
 {:user {:plugins [
                   ;; Code quality:
-                  [lein-ancient "0.6.7"]
-                  [lein-bikeshed "0.2.0"]
-                  [lein-kibit "0.1.2"]
-                  [jonase/eastwood "0.2.1"]
-                  [lein-cljfmt "0.1.10"]
-                  [venantius/yagni "0.1.1" :exclusions [org.clojure/core.cache]]
+                  ;; [lein-ancient "0.6.7"]
+                  ;; [lein-bikeshed "0.2.0"]
+                  ;; [lein-kibit "0.1.2"]
+                  ;; [jonase/eastwood "0.2.1"]
+                  ;; [lein-cljfmt "0.1.10"]
+                  ;; [venantius/yagni "0.1.1" :exclusions [org.clojure/core.cache]]
 
                   ;; Cider (Emacs):
                   [cider/cider-nrepl "0.9.1"]
-                  ;; [refactor-nrepl "1.1.0"]
+                  [refactor-nrepl "1.0.5"]
                   ;; [joodie/clojure-refactoring "0.6.4" :exclusions [org.clojure/clojure]]
 
                   ;; Android
@@ -18,23 +18,26 @@
                   ;; Other
                   [venantius/ultra "0.3.4"]
                   ;; [lein-marginalia "0.8.0"]
-                  [lein-try "0.4.3"]
-                  
-                  ]
+                  [lein-try "0.4.3"]]
 
-        :dependencies [[com.cemerick/piggieback "0.2.1"]
+        :dependencies [;; [com.cemerick/piggieback "0.2.1"]
+                       ;; [slamhound "1.5.5"]
                        [org.clojure/tools.nrepl "0.2.10"]
-                       [slamhound "1.5.5"]]
-        ; :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+                       ]
+        
+        ;; :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
 
-        :aliases {"slamhound" ["run" "-m" "slam.hound"]
-                  "analyze"   ["yagni"]
-                  "qa"        ["do" ["clean"] ["with-profile" "production" "deps" ":tree"] ["ancient"] ["kibit"] ["bikeshed"] ["cljfmt check"] ["yagni"]]}
+        ;; :aliases {"slamhound" ["run" "-m" "slam.hound"]
+        ;;           "analyze"   ["yagni"]
+        ;;           "qa"        ["do" ["clean"] ["with-profile" "production" "deps" ":tree"] ["ancient"] ["kibit"] ["bikeshed"] ["cljfmt check"] ["yagni"]]}
 
+        :jvm-opts ["-XX:MaxPermSize=128M"]
+        
         :ultra {:repl true
                 :stacktraces true
                 :tests false
                 :java true
                 :color-scheme :solarized_dark}}
 
- :cuttle {:plugins [[lein-pprint "1.1.1"]]}}
+ ;; :cuttle {:plugins [[lein-pprint "1.1.1"]]}
+ }
