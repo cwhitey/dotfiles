bb0080a3b5d44eb5827bb0ace25a93b15ee09cef
Callum White <callumw1991@gmail.com>
2015-11-30 23:54:59 +1100

    Updates to prelude config

---
+++ emacs-prelude/personal/helm.el
@@ -0,0 +1,8 @@
+(prelude-require-packages '(cljr-helm))
+
+(require 'cljr-helm)
+
+(eval-after-load 'clojure-mode
+  '(progn
+     (add-hook 'clojure-mode-hook (lambda ()
+                                    (global-set-key (kbd "C-c C-r") 'cljr-helm)))))
