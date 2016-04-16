79ab47f63eb9178112a0dadba204e46832b1a755
Callum White <callumw1991@gmail.com>
2016-04-16 13:08:50 +1000

    Clojure user profile dependency versions updated

--- leiningen/profiles.clj
+++ leiningen/profiles.clj
@@ -1,18 +1,18 @@
 {:user {:plugins [
                   ;; Code quality:
-                  ;; [lein-ancient "0.6.7"]
+                  [lein-ancient "0.6.7"]
                   ;; [lein-bikeshed "0.2.0"]
                   ;; [lein-kibit "0.1.2"]
                   ;; [jonase/eastwood "0.2.1"]
                   ;; [lein-cljfmt "0.1.10"]
-                  ;; [venantius/yagni "0.1.1" :exclusions [org.clojure/core.cache]]
+                  ;; [venantius/yagni "0.1.4"]
 
                   ;; Android
                   ;; [lein-droid "0.4.0-alpha4"] ;;Clojure android project builder
                   
                   ;; Other
-                  [venantius/ultra "0.3.4"]
-                  ;; [lein-marginalia "0.8.0"]
+                  [venantius/ultra "0.4.1"]
+                  ;; [lein-marginalia "0.9.0"]
                   [lein-try "0.4.3"]]
 
         :dependencies [;; [com.cemerick/piggieback "0.2.1"]
@@ -35,9 +35,9 @@
                 :color-scheme :solarized_dark}}
  :repl {:plugins [
                   ;; Cider (Emacs):
-                  [cider/cider-nrepl "0.9.1"]
-                  [refactor-nrepl "1.0.5"]
+                  [refactor-nrepl "2.2.0"]
+                  [cider/cider-nrepl "0.11.0"]
                   ;; [joodie/clojure-refactoring "0.6.4" :exclusions [org.clojure/clojure]]
                   ]}
  ;; :cuttle {:plugins [[lein-pprint "1.1.1"]]}
- }
+}
