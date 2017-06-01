50b313ac2d3836cfd0aaea0eb4155c6b7d646e14
cwhitey <callumw1991@gmail.com>
2017-06-02 09:26:41 +1000

    Partially fix issues with 'pure' theme by selecting the version

--- zsh/antibody/bundle-antibody.sh
+++ zsh/antibody/bundle-antibody.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 antibody bundle < "bundles.txt" > ~/.antibody-bundled-bundles.txt
-antibody bundle sindresorhus/pure >> ~/.antibody-bundled-bundles.txt
+# Use version tag of pure that is NOT broken. Git info disappears for any later version.
+antibody bundle sindresorhus/pure branch:v1.5.0 >> ~/.antibody-bundled-bundles.txt
 # Some special packages have to be loaded last
 antibody bundle < "last_bundles.txt" >> ~/.antibody-bundled-bundles.txt
