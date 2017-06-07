c9479ae39a7267dc1960483e3f6dc469ae555f3d
cwhitey <callumw1991@gmail.com>
2017-06-07 17:41:16 +1000

    Comment out other zsh theme options

--- zsh/antibody/bundle-antibody.sh
+++ zsh/antibody/bundle-antibody.sh
@@ -3,5 +3,7 @@
 antibody bundle < "bundles.txt" > ~/.antibody-bundled-bundles.txt
 # Use version tag of pure that is NOT broken. Git info disappears for any later version.
 antibody bundle sindresorhus/pure branch:v1.5.0 >> ~/.antibody-bundled-bundles.txt
+# antibody bundle dfurnes/purer >> ~/.antibody-bundled-bundles.txt
+# antibody bundle marszall87/lambda-pure >> ~/.antibody-bundled-bundles.txt
 # Some special packages have to be loaded last
 antibody bundle < "last_bundles.txt" >> ~/.antibody-bundled-bundles.txt
