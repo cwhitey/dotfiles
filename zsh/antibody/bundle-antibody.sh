1fb5d85866ded43232a4614475345113e7b440f3
Callum White <callumw1991@gmail.com>
2017-11-16 12:35:34 +1100

    Use optimised shell syntax highlighting instead

--- zsh/antibody/bundle-antibody.sh
+++ zsh/antibody/bundle-antibody.sh
@@ -2,7 +2,7 @@
 
 antibody bundle < "bundles.txt" > ~/.antibody-bundled-bundles.txt
 # Use version tag of pure that is NOT broken. Git info disappears for any later version.
-antibody bundle sindresorhus/pure branch:v1.5.0 >> ~/.antibody-bundled-bundles.txt
+# antibody bundle sindresorhus/pure >> ~/.antibody-bundled-bundles.txt
 # antibody bundle dfurnes/purer >> ~/.antibody-bundled-bundles.txt
 # antibody bundle marszall87/lambda-pure >> ~/.antibody-bundled-bundles.txt
 # Some special packages have to be loaded last
---
+++ zsh/antibody/install-pure-theme.zsh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+ln -s "$PWD/pure.zsh" /usr/local/share/zsh/site-functions/prompt_pure_setup
+ln -s "$PWD/async.zsh" /usr/local/share/zsh/site-functions/async
