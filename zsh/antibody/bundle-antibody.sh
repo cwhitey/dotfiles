8f819cf18c82dcbf2b4ff83825c72422586a2bea
Callum White <callumw1991@gmail.com>
2017-11-17 14:38:23 +1100

    Add gitit plugin

--- zsh/antibody/bundle-antibody.sh
+++ zsh/antibody/bundle-antibody.sh
@@ -1,9 +1,25 @@
 #!/bin/sh
 
-antibody bundle < "bundles.txt" > ~/.antibody-bundled-bundles.txt
-# Use version tag of pure that is NOT broken. Git info disappears for any later version.
-# antibody bundle sindresorhus/pure >> ~/.antibody-bundled-bundles.txt
-# antibody bundle dfurnes/purer >> ~/.antibody-bundled-bundles.txt
-# antibody bundle marszall87/lambda-pure >> ~/.antibody-bundled-bundles.txt
+BUNDLE_FILE="./antibody-bundled-bundles.txt"
+touch $BUNDLE_FILE
+# antibody bundle < "bundles.txt" > $BUNDLE_FILE
+# antibody bundle chrissicool/zsh-256color >> $BUNDLE_FILE
+antibody bundle zsh-users/zsh-completions >> $BUNDLE_FILE
+antibody bundle caarlos0/zsh-open-github-pr >> $BUNDLE_FILE
+antibody bundle peterhurford/git-it-on.zsh >> $BUNDLE_FILE
+# antibody bundle tcnksm/docker-alias >> $BUNDLE_FILE
+# antibody bundle greymd/docker-zsh-completion >> $BUNDLE_FILE
+antibody bundle hlissner/zsh-autopair >> $BUNDLE_FILE
+# antibody bundle Tarrasch/zsh-bd >> $BUNDLE_FILE
+# antibody bundle zsh-users/zaw >> $BUNDLE_FILE
+# antibody bundle zsh-users/zsh-autosuggestions >> $BUNDLE_FILE
+
+# Themes
+# antibody bundle sindresorhus/pure >> $BUNDLE_FILE
+# antibody bundle dfurnes/purer >> $BUNDLE_FILE
+# antibody bundle marszall87/lambda-pure >> $BUNDLE_FILE
+
 # Some special packages have to be loaded last
-antibody bundle < "last_bundles.txt" >> ~/.antibody-bundled-bundles.txt
+# antibody bundle < "last_bundles.txt" >> $BUNDLE_FILE
+antibody bundle zdharma/fast-syntax-highlighting >> $BUNDLE_FILE
+antibody bundle zsh-users/zsh-history-substring-search >> $BUNDLE_FILE
