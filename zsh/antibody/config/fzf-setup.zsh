d47a0db8b1f0fa39cc1e8f4cc383af022b95edb1
Callum White <callumw1991@gmail.com>
2017-11-20 13:29:00 +1100

    Silence fzf binding output

--- zsh/antibody/config/fzf-setup.zsh
+++ zsh/antibody/config/fzf-setup.zsh
@@ -1,7 +1,7 @@
 # Dedicated completion key while retaining default TAB (^I) behaviour
 export FZF_COMPLETION_TRIGGER=''
 #bindkey '^T' fzf-completion
-bindkey '^I' $fzf_default_completion
+bindkey '^I' $fzf_default_completion >/dev/null 
 fzf_color_options="--color fg:-1,bg:-1,hl:230,fg+:3,bg+:233,hl+:229 --color info:150,prompt:110,spinner:150,pointer:167,marker:174"
 export FZF_DEFAULT_OPTS="--exact --cycle --reverse --height 12 --inline-info --bind=ctrl-j:accept,ctrl-k:kill-line $fzf_color_options"
 # Use the fast the_platinum_surfer instead of find. Also respects .gitignore etc.
