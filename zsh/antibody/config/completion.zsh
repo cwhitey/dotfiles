52eee17c46027fc5d2e84434f72a26de9c6b8b54
Callum White <callumw1991@gmail.com>
2017-11-23 11:06:39 +1100

    Change to completion.zsh

--- zsh/antibody/config/completion.zsh
+++ zsh/antibody/config/completion.zsh
@@ -1,4 +1,4 @@
-zstyle ':completion:*:*:*:*:*' menu select
+zstyle ':completion:*' menu select
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
 zstyle ':completion:*' list-colors ''
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
