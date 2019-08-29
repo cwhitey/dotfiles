ff6f0e3e9734b38977db7ffc38c5253c7dc91761
Callum White <callumw1991@gmail.com>
2019-08-29 14:00:27 +1000

    Lein profile changes

--- leiningen/profiles.clj
+++ leiningen/profiles.clj
@@ -1,17 +1,22 @@
-{:user {:plugins [;; Code quality:
-                  [lein-ancient "0.6.14"]
-                  [lein-bikeshed "0.2.0"]
-                  [lein-kibit "0.1.2"]
-                  [jonase/eastwood "0.2.1"]
-                  [lein-cljfmt "0.1.10"]
-                  [lein-pprint "1.2.0"]
-                  [venantius/yagni "0.1.4"]
+{:user {:plugins [
+
+                  ;; Code quality:
+                  ;; [lein-bikeshed "0.5.0" :exclusions [org.clojure/tools.namespace]]
+                  ;; [lein-kibit "0.1.6"]
+                  ;; [jonase/eastwood "0.3.5"]
+                  ;; [lein-cljfmt "0.6.4"]
+                  ;; [venantius/yagni "0.1.7"]
+
+
                   ;; Other
-                  ;;[venantius/ultra "0.5.2"]
-                  [com.billpiel/sayid "0.0.16"]
-                  [lein-try "0.4.3"]]
-        :dependencies [[org.clojure/clojure "1.9.0"] ;default clojure version for repls
-                       [org.clojure/tools.nrepl "0.2.13"]
+                  [lein-ancient "0.6.14"]
+                  [lein-try "0.4.3"]
+                  [lein-nvd "1.0.0" :exclusions [org.slf4j/jcl-over-slf4j
+                                                 org.slf4j/slf4j-api]]
+                  ;;[venantius/ultra "0.6.0"]
+
+                  ]
+        :dependencies [[org.clojure/clojure "1.10.0"] ;default clojure version for repls
                        ;;[spyscope "0.1.6"]
                        ]
         ;;:injections [(require 'spyscope.core)]
@@ -21,9 +26,4 @@
         ;;         :java true
         ;;         :color-scheme :solarized_dark}
         ;; :jvm-opts ["-XX:MaxPermSize=128M"]
-        }
- :repl {:plugins [
-                  [cider/cider-nrepl "0.17.0"]
-                  [refactor-nrepl "2.4.0-snapshot"]
-                  ;; [joodie/clojure-refactoring "0.6.4" :exclusions [org.clojure/clojure]]
-                  ]}}
+        }}
